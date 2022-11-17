6e434c00d83610bd35084c2fdcc8102f7de03fb1
Sanya <a.kondratev9@gmail.com>
2022-11-17 15:41:15 +0300

    вписана задача 2

--- Program.cs
+++ Program.cs
@@ -4,17 +4,34 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-Console.WriteLine("Введите первое число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int b = Convert.ToInt32(Console.ReadLine());
-if (a > b)
-{
-    Console.WriteLine($"Большее число:{a}, Меньшее число: {b}");
-}
-else if (a < b)
-{
-    Console.WriteLine($"Большее число:{b}, Меньшее число: {a}");
-}
-else
-Console.WriteLine("Числа равны");
+// Console.WriteLine("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a > b)
+// {
+//     Console.WriteLine($"Большее число:{a}, Меньшее число: {b}");
+// }
+// else if (a < b)
+// {
+//     Console.WriteLine($"Большее число:{b}, Меньшее число: {a}");
+// }
+// else
+// Console.WriteLine("Числа равны");
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+// Console.WriteLine($"максимальное число:{max}");
