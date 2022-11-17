12dcafe1a6f1fd52af216bca1effda1d8c3df5ff
Sanya <a.kondratev9@gmail.com>
2022-11-17 15:16:28 +0300

    Первая задача

--- Program.cs
+++ Program.cs
@@ -3,3 +3,18 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine($"Большее число:{a}, Меньшее число: {b}");
+}
+else if (a < b)
+{
+    Console.WriteLine($"Большее число:{b}, Меньшее число: {a}");
+}
+else
+Console.WriteLine("Числа равны");
